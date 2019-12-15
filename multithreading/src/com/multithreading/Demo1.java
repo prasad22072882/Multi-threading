aceb1473c43a1fbd9677cd6f3f6eabf0930faacd
prasad <prasad22072882@gmail.com>
2019-12-15 18:43:49 +0530

    Reduce the no. of lines of code using lambda expressions

--- multithreading/src/com/multithreading/Demo1.java
+++ multithreading/src/com/multithreading/Demo1.java
@@ -2,37 +2,29 @@ package com.multithreading;
 
 public class Demo1 {
 	public static void main(String[] args) throws InterruptedException {
-		Thread t1 = new Thread(new A());
-		Thread t2 = new Thread(new B());
+		Thread t1 = new Thread(() -> {
+			for (int i = 1; i <= 5; i++) {
+				System.out.println("A " + i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		Thread t2 = new Thread(() -> {
+			for (int i = 1; i <= 5; i++) {
+				System.out.println("B " + i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
 		t1.start();
 		Thread.sleep(10);
 		t2.start();
-	}
-}
-
-class A implements Runnable {
-	public void run() {
-		for (int i = 1; i <= 5; i++) {
-			System.out.println("A " + i);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-}
-
-class B implements Runnable {
-	public void run() {
-		for (int i = 1; i <= 5; i++) {
-			System.out.println("B " + i);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
